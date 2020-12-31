ab3236e9550359dd452ea09c66ad07a558bfc359
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-12-31 15:20:22 +0900

    planscontrollerの作成

---
+++ backend/app/Http/Controllers/PlansController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlansController extends Controller
+{
+    public function index() {
+        return view('plans.index', ['plans' => $plans]);
+    }
+}
