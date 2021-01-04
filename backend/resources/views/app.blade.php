2561672f819c52628d11a1913612a7d964f14609
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-01-04 17:55:00 +0900

    app.blade.phpにbootstrapのCDNを追加

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -46,5 +46,8 @@
     
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 </body>
 </html>
