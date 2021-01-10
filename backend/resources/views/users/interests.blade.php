85fa5086db64e0e2ff782a34f6159e493dfcf390
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-01-10 13:22:56 +0900

    タブの共通化

--- backend/resources/views/users/interests.blade.php
+++ backend/resources/views/users/interests.blade.php
@@ -6,18 +6,7 @@
   @include('nav')
   <div class="container">
     @include('users.user')
-    <ul class="nav nav-tabs nav-justified mt-3">
-      <li class="nav-item">
-        <a class="nav-link text-muted" href="{{ route('users.show', ['name' => $user->name]) }}">
-          投稿したオフ会
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-muted active" href="{{ route('users.interests', ['name' => $user->name]) }}">
-          興味あり！
-        </a>
-      </li>
-    </ul>
+    @include('users.tabs', ['hasPlans' => false, 'hasInterests' => true])
     @foreach($plans as $plan)
       @include('plans.card')
     @endforeach
--- backend/resources/views/users/show.blade.php
+++ backend/resources/views/users/show.blade.php
@@ -5,40 +5,10 @@
 @section('content')
   @include('nav')
   <div class="container">
-    <div class="card mt-3">
-      <div class="card-body">
-        <div class="d-flex flex-row">
-          <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-            <i class="fas fa-user-circle fa-3x"></i>
-          </a>
-        </div>
-        <h2 class="h5 card-title m-0">
-          <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-            {{ $user->name }}
-          </a>
-        </h2>
-      </div>
-      <div class="card-body">
-        <div class="card-text">
-        </div>
-      </div>
-    </div>
-
-    <ul class="nav nav-tabs nav-justified mt-3">
-      <li class="nav-item">
-        <a class="nav-link text-muted active" href="{{ route('users.show', ['name' => $user->name]) }}">
-          投稿したプラン
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-muted" href="">
-          興味あり！
-        </a>
-      </li>
-    </ul>
+    @include('users.user')
+    @include('users.tabs', ['hasPlans' => true, 'hasInterests' => false])
     @foreach($plans as $plan)
       @include('plans.card')
     @endforeach
-
   </div>
 @endsection
