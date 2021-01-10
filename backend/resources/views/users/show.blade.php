d2018c5a0e6cd16137f1e6629e125ee7f5a7a0cb
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-01-10 11:59:19 +0900

    user詳細ページに投稿したプランと興味あり！のプランを表示

--- backend/resources/views/users/show.blade.php
+++ backend/resources/views/users/show.blade.php
@@ -23,5 +23,22 @@
         </div>
       </div>
     </div>
+
+    <ul class="nav nav-tabs nav-justified mt-3">
+      <li class="nav-item">
+        <a class="nav-link text-muted active" href="{{ route('users.show', ['name' => $user->name]) }}">
+          投稿したプラン
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-muted" href="">
+          興味あり！
+        </a>
+      </li>
+    </ul>
+    @foreach($plans as $plan)
+      @include('plans.card')
+    @endforeach
+
   </div>
 @endsection
