b662a3b889feb2b0fbde618abc24f891e6df9acd
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-01-12 15:54:45 +0900

    ルーティングの作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlanController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\LoginController;
 
@@ -49,6 +50,9 @@ Route::prefix('users')->name('users.')->group(function () {
 
 #ゲストログイン
 Route::get('guest', [LoginController::class, 'guestLogin'])->name('login.guest');
+
+#コメント
+Route::resource('/comments', CommentController::class)->only(['store']);
 
 #ホーム
 Route::get('/home', [HomeController::class, 'index'])->name('home');
