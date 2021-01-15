1db52e62b19de6676a60403f2ce798fa3b2619df
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-01-15 15:44:05 +0900

    フッター下の余白を修正

--- backend/resources/views/auth/register.blade.php
+++ backend/resources/views/auth/register.blade.php
@@ -83,7 +83,7 @@
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-block mt-4 mb-2" style="background-color: rgb(	0,200,179); color: #fff;">
                                         登録
                                     </button>
                                 </div>
