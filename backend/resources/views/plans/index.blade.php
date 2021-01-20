418dec09dbdd4a3ad7ac72a1048a2c7ec019c899
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-01-20 14:10:53 +0900

    ページネーションのUIの整え

--- backend/resources/views/plans/index.blade.php
+++ backend/resources/views/plans/index.blade.php
@@ -10,7 +10,7 @@
         @foreach($plans as $plan)
           @include('plans.smallcard')
         @endforeach
-        {{ $plans->links() }}
+        {{ $plans->links('pagination::default') }}
       </div>
     </div>
   </div>
