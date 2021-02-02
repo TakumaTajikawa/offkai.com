41db33c1f66a3d0ec616d48b5a46c70b9a829ab9
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-02 22:13:10 +0900

    PlanTestとUserTestでクラスをインポート

--- backend/tests/Feature/UserTest.php
+++ backend/tests/Feature/UserTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class UserTest extends TestCase
 {
@@ -17,16 +18,16 @@ class UserTest extends TestCase
      */
     public function testUserRegistrationTest()
     {
-        $user = new \App\Models\User;
+        $user = new User;
         $user->name = "山田";
         $user->gender = "男性";
         $user->email = "yamada@test.com";
         $user->password = \Hash::make('password');
         $user->save();
 
-        $readUser = \App\Models\User::where('name', '山田')->first();
+        $readUser = User::where('name', '山田')->first();
         $this->assertNotNull($readUser);
         $this->assertTrue(\Hash::check('password', $readUser->password));
-        \App\Models\User::where('email', 'yamada@test.com')->delete();
+        User::where('email', 'yamada@test.com')->delete();
     }
 }
